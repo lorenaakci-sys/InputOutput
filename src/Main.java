41390d0cf8951d91b4bd11d7a21caccf542d0c3c
Lorena <lorena.akci@gmail.com>
2026-09-14 20:45:16 +0200

    ----

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite putanju:");
+        String originalPath = scanner.nextLine();
+        System.out.println("Unesite naziv kopije:");
+        String copyPath = scanner.nextLine();
+
         try {
-            FileInputStream input = new FileInputStream("keywords.pdf");
-            FileOutputStream output = new FileOutputStream("kopija.pdf");
+            FileInputStream input = new FileInputStream(originalPath);
+            FileOutputStream output = new FileOutputStream(copyPath);
 
             int data;
 
@@ -23,7 +30,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        File copy = new File("kopija.pdf");
+        File copy = new File(copyPath);
         if (copy.exists()) {
             copy.delete();
         }
